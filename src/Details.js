498802e49ba4449d460b49dcf85fe4eaaa6dc5d5
Suyash Chandra <suyash.chandra24@gmail.com>
2020-03-31 15:52:53 +0530

    Context with Class

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
 import ErrorBoundary from './ErrorBoundary';
+import ThemeContext from './ThemeContext';
 
 class Details extends React.Component {
   state = { loading: true };
@@ -25,17 +26,22 @@ class Details extends React.Component {
     }
     const { name, animal, location, description, breed, media } = this.state;
     return (
-      <ErrorBoundary>
-        <div className="details">
-          <Carousel media={media} />
-          <div>
-            <h1>{name}</h1>
-            <h2>{`${animal} - ${breed} - ${location}`}</h2>
-            <button>Adopt {name}</button>
-            <p>{description}</p>
-          </div>
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <ThemeContext.Consumer>
+            {themeHook => (
+              <button style={{ backgroundColor: themeHook[0] }}>
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
+
+          <p>{description}</p>
         </div>
-      </ErrorBoundary>
+      </div>
     );
   }
 }
