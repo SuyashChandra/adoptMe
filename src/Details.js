63924b3e47900130756ce72b1f5f8ec9e53ce38f
Suyash Chandra <suyash.chandra24@gmail.com>
2020-03-31 14:58:23 +0530

    Made Error Boundries as midleware around Details

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
 
 class Details extends React.Component {
   state = { loading: true };
   componentDidMount() {
+    throw new Error('lol');
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -24,17 +26,25 @@ class Details extends React.Component {
     }
     const { name, animal, location, description, breed, media } = this.state;
     return (
-      <div className="details">
-        <Carousel media={media} />
-        <div>
-          <h1>{name}</h1>
-          <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
-          <p>{description}</p>
+      <ErrorBoundary>
+        <div className="details">
+          <Carousel media={media} />
+          <div>
+            <h1>{name}</h1>
+            <h2>{`${animal} - ${breed} - ${location}`}</h2>
+            <button>Adopt {name}</button>
+            <p>{description}</p>
+          </div>
         </div>
-      </div>
+      </ErrorBoundary>
     );
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
